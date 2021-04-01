00b03d552f31873266b4e1d0a6c88bef6e115bbc
capricornus44 <antonzavalniuk@gmail.com>
2021-04-01 17:29:36 +0200

    remove id from component's parameters

--- src/components/FriendList/Friend.js
+++ src/components/FriendList/Friend.js
@@ -2,10 +2,10 @@ import React from "react"
 import PropTypes from "prop-types"
 import styles from "./FriendList.module.css"
 
-const Friend = ({ avatar, name, isOnline, id }) => {
+const Friend = ({ avatar, name, isOnline }) => {
   const status = isOnline ? styles.online : styles.offline
   return (
-    <li className={styles.item} key={id}>
+    <li className={styles.item}>
       <span className={status} />
       <img className={styles.avatar} src={avatar} alt={name} width="48" />
       <p className={styles.name}>{name}</p>
