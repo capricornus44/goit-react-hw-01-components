ea6d3971b21eaa4d6442daa56f08d96db66159a8
capricornus44 <antonzavalniuk@gmail.com>
2021-04-01 08:29:37 +0200

    task-01. Profile

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from "react"
+import Profile from "./components/Profile/Profile"
+import Statistics from "./components/Statistics/Statistics"
+import FriendList from "./components/FriendList/FriendList"
+import TransactionHistory from "./components/TransactionHistory/TransactionHistory"
+
+import user from "./db/user.json"
+import statisticalData from "./db/statistical-data.json"
+import friends from "./db/friends.json"
+import transactions from "./db/transactions.json"
+
+const App = () => (
+  <>
+    <Profile name={user.name} tag={user.tag} location={user.location} avatar={user.avatar} stats={user.stats} />
+    <Statistics title="Upload stats" stats={statisticalData} />
+    <FriendList friends={friends} />
+    <TransactionHistory items={transactions} />;
+  </>
+)
+
+export default App
---
+++ src/components/Profile/Profile.js
@@ -0,0 +1,52 @@
+import React from "react"
+import PropTypes from "prop-types"
+
+import styles from "./Profile.module.css"
+import defaultImage from "./default.jpg"
+
+const Profile = ({ avatar, name, tag, location, stats }) => {
+  const { followers, views, likes } = stats
+  return (
+    <div className={styles.profile}>
+      <div className={styles.description}>
+        <img src={avatar} alt="Аватар пользователя" className={styles.avatar} />
+        <p className={styles.name}>{name}</p>
+        <p className={styles.tag}>@{tag}</p>
+        <p className={styles.location}>{location}</p>
+      </div>
+
+      <ul className={styles.stats}>
+        <li className={styles.item}>
+          <span className={styles.label}>Followers</span>
+          <span className={styles.quantity}>{followers}</span>
+        </li>
+        <li className={styles.item}>
+          <span className={styles.label}>Views</span>
+          <span className={styles.quantity}>{views}</span>
+        </li>
+        <li className={styles.item}>
+          <span className={styles.label}>Likes</span>
+          <span className={styles.quantity}>{likes}</span>
+        </li>
+      </ul>
+    </div>
+  )
+}
+
+Profile.defaultProps = {
+  avatar: defaultImage,
+}
+
+Profile.propTypes = {
+  avatar: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  tag: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  stats: PropTypes.shape({
+    followers: PropTypes.number.isRequired,
+    views: PropTypes.number.isRequired,
+    likes: PropTypes.number.isRequired,
+  }),
+}
+
+export default Profile
